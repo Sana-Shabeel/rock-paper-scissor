fddad700bb5041ece76c7db3039be2a06db6a56f
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-15 09:55:58 +0300

    Add responsivity to score board

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const ScoreBoard = () => {
   return (
-    <header className="w-11/12 h-24 border-2 mx-auto border-headerOutline flex justify-between items-center px-4">
-      <div className="w-20">
+    <header className="w-11/12 h-24 border-2 mx-auto border-headerOutline flex justify-between items-center px-4 md:w-4/5 lg:w-8/12 lg:h-36  ">
+      <div className="w-20 lg:w-40">
         <img src="/images/logo.svg" alt="logo" />
       </div>
 
-      <div className="w-20 h-[4.5rem] border bg-scoreBoard text-center grid place-items-center rounded-md">
+      <div className="w-20 h-[4.5rem] border bg-scoreBoard text-center grid place-items-center rounded-md lg:w-36 lg:h-auto lg:py-3">
         <div>
-          <p className="text-scoreText text-xs">SCORE</p>
-          <h1 className="text-darkText text-4xl font-bold">12</h1>
+          <p className="text-scoreText text-xs lg:text-base">SCORE</p>
+          <h1 className="text-darkText text-4xl font-bold lg:text-6xl">12</h1>
         </div>
       </div>
     </header>
