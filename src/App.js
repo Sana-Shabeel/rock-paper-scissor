1b1fa8968a23c3daaba57039be1c796907084b82
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-15 10:46:18 +0300

    Add layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Layout from "./components/Layout";
 import ScoreBoard from "./components/ScoreBoard";
 
 function App() {
   return (
     <div className="app min-h-screen bg-gradient-to-r from-radialGradient-100 to-radialGradient-200 pt-6 font-barlow">
       <ScoreBoard />
+      <Layout />
     </div>
   );
 }
