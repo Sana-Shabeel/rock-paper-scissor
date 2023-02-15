daf57adcbca532095b35e058e506a172f2e53c9c
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-15 21:16:07 +0300

    Add Util

---
+++ src/Utils/Classes.js
@@ -0,0 +1,14 @@
+export const classes = {
+  paper: {
+    div: "border-blue shadow-paper",
+    img: "/images/icon-paper.svg",
+  },
+  rock: {
+    div: "border-red shadow-rock",
+    img: "/images/icon-rock.svg",
+  },
+  scissor: {
+    div: "border-yellow shadow-scissor",
+    img: "/images/icon-scissors.svg",
+  },
+};
