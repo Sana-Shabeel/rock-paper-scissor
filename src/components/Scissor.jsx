d1180041ea254a7ac5ab1b3413808aad7f82ebb0
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-15 10:45:38 +0300

    Add scissor  component

---
+++ src/components/Scissor.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Scissor = () => {
+  return (
+    <div className="  m-6">
+      <div className="bg-white w-36 h-36  rounded-full grid place-items-center  border-[1.3rem] border-red shadow-6xl cursor-pointer md:w-48 md:h-48">
+        <img src="/images/icon-rock.svg" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Scissor;
