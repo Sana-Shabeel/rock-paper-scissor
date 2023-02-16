bd6ad7b7b7a2db8171f55c5daa5da4a079dd357e
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-16 13:10:01 +0300

    Add paper

--- src/components/Paper.jsx
+++ src/components/Paper.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Paper = () => {
+const Paper = ({ setplayerChoice }) => {
   return (
-    <div className="  m-6">
-      <div className="bg-white w-36 h-36 rounded-full grid place-items-center  border-[1.3rem] border-blue shadow-6xl cursor-pointer md:w-48 md:h-48">
-        <img src="/images/icon-rock.svg" alt="" />
+    <div className="  m-6" onClick={() => setplayerChoice("paper")}>
+      <div className="bg-white w-36 h-36 rounded-full grid place-items-center  border-[1.3rem] border-blue shadow-4xl cursor-pointer md:w-48 md:h-48">
+        <img src="/images/icon-paper.svg" alt="paper" />
       </div>
     </div>
   );
