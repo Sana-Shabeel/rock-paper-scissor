bffb6c418ae771b56f73ac8d60f699533ce120bb
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-16 13:09:28 +0300

    Show result

--- src/components/HandOption.jsx
+++ src/components/HandOption.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 
-const HandOption = ({ setPlayerchoice, className, img }) => {
-  console.log("HandOption", className);
+const HandOption = ({ setPlayerchoice, className, img, sign }) => {
+  // console.log("HandOption", className);
   return (
     <div
-      className="  m-6"
+      className=""
       onClick={() =>
         setPlayerchoice({
           className: className,
           img: img,
+          sign: sign,
         })
       }
     >
       <div
-        className={`${className} bg-white w-36 h-36 rounded-full grid place-items-center  border-[1.3rem] shadow-4xl cursor-pointer md:w-48 md:h-48`}
+        className={`${className} bg-white w-32 h-32 rounded-full grid place-items-center  border-[1.3rem] shadow-4xl cursor-pointer md:w-40 md:h-40 lg:w-48 lg:h-48`}
       >
         <img src={img} alt="paper" />
       </div>
