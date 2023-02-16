822563bf43e13aa02da949c7de9259e640833b19
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-16 13:09:42 +0300

    Show result

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,39 @@
 import React from "react";
-import Paper from "./Paper";
-import Rock from "./Rock";
-import Scissor from "./Scissor";
+import { Link } from "react-router-dom";
+import HandOption from "./HandOption";
+import { classes } from "../Utils/Classes";
 
-const Layout = () => {
+const Layout = ({ setPlayerchoice }) => {
   return (
-    <div className="w-11/12 mx-auto">
-      <div className="flex justify-around">
-        <Paper />
-        <Scissor />
+    <div className="w-11/12 mx-auto mt-6 md:w-3/5 ">
+      <div className="flex justify-around mb-6">
+        <Link to={"/result"}>
+          <HandOption
+            setPlayerchoice={setPlayerchoice}
+            className={classes.paper.div}
+            img={classes.paper.img}
+            sign="paper"
+          />
+        </Link>
+
+        <Link to={"/result"}>
+          <HandOption
+            setPlayerchoice={setPlayerchoice}
+            className={classes.rock.div}
+            img={classes.rock.img}
+            sign="rock"
+          />
+        </Link>
       </div>
       <div className="flex justify-center">
-        <Rock />
+        <Link to={"/result"}>
+          <HandOption
+            setPlayerchoice={setPlayerchoice}
+            className={classes.scissor.div}
+            img={classes.scissor.img}
+            sign="scissor"
+          />
+        </Link>
       </div>
     </div>
   );
