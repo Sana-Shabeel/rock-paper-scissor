85a9fb119f65a2e782c44a8ed7e34ecc58ebb1b0
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-17 07:05:42 +0300

    Add score functionality

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
-const ScoreBoard = () => {
+const ScoreBoard = ({ score }) => {
   return (
-    <header className="w-11/12 h-24 border-2 mx-auto border-headerOutline flex justify-between items-center px-4 md:w-4/5 lg:w-8/12 lg:h-36  ">
+    <header className="mx-auto flex h-24 w-11/12 items-center justify-between border-2 border-headerOutline px-4 md:w-4/5 lg:h-36 lg:w-8/12  ">
       <div className="w-20 lg:w-40">
         <img src="/images/logo.svg" alt="logo" />
       </div>
 
-      <div className="w-20 h-[4.5rem] border bg-scoreBoard text-center grid place-items-center rounded-md lg:w-36 lg:h-auto lg:py-3">
+      <div className="grid h-[4.5rem] w-20 place-items-center rounded-md border bg-scoreBoard text-center lg:h-auto lg:w-36 lg:py-3">
         <div>
-          <p className="text-scoreText text-xs lg:text-base">SCORE</p>
-          <h1 className="text-darkText text-4xl font-bold lg:text-6xl">12</h1>
+          <p className="text-xs text-scoreText lg:text-base">SCORE</p>
+          <h1 className="text-4xl font-bold text-darkText lg:text-6xl">
+            {score}
+          </h1>
         </div>
       </div>
     </header>
